77789c28f502342b408bc59dc8d63305648089fb
Shidul Islam <sishahin093@gmail.com>
2022-03-05 08:13:12 +0600

    39-2 Browser alert, confirm, prompt with examples

---
+++ alert.js
@@ -0,0 +1,24 @@
+// console.log('i am hear');
+
+// alert('ma is comming');
+const maComing = () => {
+    alert('ammu  rajshahi thkee astese open the books');
+}
+
+
+const askPicnic = () => {
+    const res = confirm('are you going to picnic');
+    if (res === true) {
+        alert('amk feee ta bolonh please !!!!');
+    } else {
+        console.log("dure giya mor");
+    }
+}
+
+const askName = () => {
+    const name = prompt('what is your name?');
+    console.log(name);
+    if (name === true) {
+        console.log(name);
+    }
+}
